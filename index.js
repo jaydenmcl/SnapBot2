114ec58f3749df6107770fd740ba2067ddb0de58
Emmanuel Rodrigues <alwesor@gmail.com>
2025-05-26 15:06:22 +0530

    update index.js

--- index.js
+++ index.js
@@ -10,8 +10,28 @@ let credentials = {
 };
 
 async function sendSnap() {
-  await bot.launchSnapchat({ headless: false }); // makes the browser visible
-  await bot.login(credentials);
+  await bot.launchSnapchat({
+    headless: false, // makes the browser visible
+    args: [
+      "--start-maximized",
+      "--force-device-scale-factor=1",
+      "--allow-file-access-from-files",
+      "--use-fake-ui-for-media-stream", // Bypass permission prompt
+      "--enable-media-stream",
+    ],
+    userDataDir:
+      "C:\\Users\\your_name\\AppData\\Local\\Google\\Chrome\\User Data\\Default",
+  });
+
+  const isLogged = await bot.isLogged();
+
+  if (!isLogged) {
+    await bot.login(credentials);
+  } else {
+    console.log("Bot is already Logged in");
+  }
+
+  await bot.handlePopup();
   await bot.captureSnap({ caption: "Hello world" });
   await bot.screenshot({ path: "screenshot.png" });
   await bot.send("BestFriends");
